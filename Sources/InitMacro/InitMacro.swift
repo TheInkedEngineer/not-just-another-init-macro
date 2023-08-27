06449d8154ca7c4ac0d4034e1eb7646e822f4359
TheInkedEngineer <firas@hey.com>
2023-08-27 18:30:06 +0200

    Fix documentation

--- Sources/InitMacro/InitMacro.swift
+++ Sources/InitMacro/InitMacro.swift
@@ -5,7 +5,7 @@ import InitMacroImplementation
 /// Example:
 ///
 /// ```swift
-///  @Init
+///  @Init()
 ///  public struct Test {
 ///    let key: String
 ///  }
@@ -23,12 +23,12 @@ import InitMacroImplementation
 /// ```
 ///
 /// - Parameters:
+///   - accessLevel: The access level associated with the initialiser. A value of `InitMacro.AccessLevel`. The access level
+///                  cannot be higher than that of the enclosing object. Defaults to `.public`.
 ///   - defaultValues: A key-value object where the key represents the name of the property with which to assign a default value in the initialiser.
 ///   - exclude: An array of property names to exclude from the initialiser.
 ///              These values should be spelled correctly as those properties, and can be excluded when applicable.
 ///              You cannot exclude a non initialised property. Initialised constants will be excluded automatically.
-///   - accessLevel: The access level associated with the initialiser. A value of `InitMacro.AccessLevel`. The access level
-///                  cannot be higher than that of the enclosing object. Defaults to `.public`.
 @attached(member, names: named(init))
 public macro Init(
   accessLevel: InitMacro.AccessLevel = .public,
